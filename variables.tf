564d79448666a2e00bbbbfa92c9c88fd4e73aab3
vemashop <vemasaikumar328@gmail.com>
2025-02-11 11:00:47 +0530

    Adding all files

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "sg_name" {
+  
+}
+
+variable "sg_description" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "common_tags" {
+  
+}
+
+variable "sg_tags" {
+  default = {}
+}
